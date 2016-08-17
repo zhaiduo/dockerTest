8ed5240e576606dcce3fdb84e2141ce26e1902fd
lvyadong <lvyadong@hopperclouds.com>
2016-08-17 18:43:41 +0800

    new: 初始化文件上传server

--- README.md
+++ README.md
@@ -2,39 +2,42 @@
 
 ##How to use
 
-安装好 Docker 环境后，直接运行我们构建好的容器即可：
-docker run -d --restart=always -p 8080:8080 --name docker_node_app_alpine zhaiduo/docker_node_app:alpine
+安装好 `Docker` 环境后，直接运行我们构建好的容器即可：
+> docker run -d --restart=always -p 8080:8080 --name docker_node_app_alpine zhaiduo/docker_node_app:alpine
 
 ##More Tips
 
-创建一个容器，让其中运行 bash 应用。
-docker run -t -i zhaiduo/docker_node_app:alpine /bin/bash
+创建一个容器，让其中运行 `bash` 应用。
+> docker run -t -i zhaiduo/docker_node_app:alpine /bin/bash
 
 停止/开始运行
-docker stop/start [CONTAINER ID]/[IMAGE]
+> docker stop/start [CONTAINER ID]/[IMAGE]
 
-使用 docker commit 命令来提交更新后的副本。
-docker commit -m "Added" -a "Docker New" [CONTAINER ID] [IMAGE]
+使用 `docker commit` 命令来提交更新后的副本。
+> docker commit -m "Added" -a "Docker New" [CONTAINER ID] [IMAGE]
 
-docker tag 命令来修改镜像的标签。
-docker tag [CONTAINER ID] [IMAGE]
-docker images [IMAGE]
+`docker tag` 命令来修改镜像的标签。
+> docker tag [CONTAINER ID] [IMAGE]
+> docker images [IMAGE]
 
-docker push 命令，把自己创建的镜像上传到仓库中来共享。例如，用户在 Docker Hub 上完成注册后，可以推送自己的镜像到仓库中。
+`docker push` 命令，把自己创建的镜像上传到仓库中来共享。例如，用户在 `Docker Hub` 上完成注册后，可以推送自己的镜像到仓库中。
 
 如果要导出镜像到本地文件
-docker save -o [IMAGEName].tar [IMAGE]
-docker export [CONTAINER ID] > [IMAGEName].tar
+> docker save -o [IMAGEName].tar [IMAGE]
+> docker export [CONTAINER ID] > [IMAGEName].tar
 
 载入镜像
-docker load --input [IMAGEName].tar | docker load < [IMAGEName].tar ｜ cat [IMAGEName].tar | docker import - [IMAGE]
-docker load 来导入镜像存储文件到本地镜像库，也可以使用 docker import 来导入一个[容器快照]到本地镜像库。
+> docker load --input [IMAGEName].tar | docker load < [IMAGEName].tar ｜ cat [IMAGEName].tar | docker import - [IMAGE]
+
+`docker load` 来导入镜像存储文件到本地镜像库，也可以使用 `docker import` 来导入一个[容器快照]到本地镜像库。
 这两者的区别在于[容器快照]文件将丢弃所有的历史记录和元数据信息
 
-移除本地的镜像，可以使用 docker rmi 命令。注意 docker rm 命令是移除容器
-docker rmi [IMAGE]
-*注意：在删除镜像之前要先用 docker rm 删掉依赖于这个镜像的所有容器。
+移除本地的镜像，可以使用 `docker rmi` 命令。注意 `docker rm` 命令是移除容器
+> docker rmi [IMAGE]
 
+* 注意：在删除镜像之前要先用 `docker rm` 删掉依赖于这个镜像的所有容器。
+
+##Links
 More: https://yeasy.gitbooks.io/docker_practice/content/introduction/
 
 
--- app.js
+++ app.js
@@ -1,15 +1,75 @@
 // app.js
-'use strict';
+'use strict'
 
 const express = require('express')
+const formidable = require('formidable')
+const fs = require('fs')
+const path = require('path')
 
 // Constants
 const PORT = 8080
 
 // App
 const app = express()
-app.get('/', function (req, res) {
-  res.send('Hello world\n')
+
+// Add headers
+app.use((req, res, next) => {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8088')
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true)
+
+    // Pass to next layer of middleware
+    next()
+})
+
+app.use('/uploads', express.static('uploads'))
+
+app.get('/', function(req, res) {
+    res.send('Welcome to upload server!\n')
+})
+
+app.post('/upload', function(req, res) {
+
+    // create an incoming form object
+    var form = new formidable.IncomingForm()
+
+    // specify that we want to allow the user to upload multiple files in a single request
+    form.multiples = true
+
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, '/uploads')
+
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', function(field, file) {
+        //console.log('file', file.path, form.uploadDir, file.name);
+        fs.rename(file.path, path.join(form.uploadDir, file.name) + '.png')
+    })
+
+    // log any errors that occur
+    form.on('error', function(err) {
+        console.log('An error has occured: \n' + err)
+    })
+
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function() {
+        res.end('success')
+    })
+
+    // parse the incoming request containing the form data
+    form.parse(req)
+    //console.log('form', form);
+
 })
 
 app.listen(PORT)
