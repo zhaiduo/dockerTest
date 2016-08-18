0d0519eace3b6f9b1ddeb80124debf5c436a0d28
lvyadong <lvyadong@hopperclouds.com>
2016-08-18 14:06:59 +0800

    new: init image upload server

--- app.js
+++ app.js
@@ -5,9 +5,123 @@ const express = require('express')
 const formidable = require('formidable')
 const fs = require('fs')
 const path = require('path')
+const mkdirp = require('mkdirp')
 
 // Constants
 const PORT = 8080
+const HOST = 'localhost'
+const HTTP = 'http'
+const UPLOAD_URL = 'upload'
+const UPLOAD_DIR = 'uploads'
+const CORS_DOMAIN = 'http://localhost:8088'
+const IMG_PREFIX = 'blob_'
+const SQL_DIR = 'db'
+
+const getDateObj = (t) => {
+    let dt;
+    if (t !== undefined) {
+        if (typeof t === 'string' || typeof t === 'number') {
+            dt = new Date(t);
+        } else {
+            dt = new Date();
+        }
+    } else {
+        dt = new Date();
+    }
+    return {
+        y: dt.getFullYear(),
+        m: dt.getMonth() + 1,
+        d: dt.getDate(),
+        h: dt.getHours(),
+        i: dt.getMinutes(),
+        s: dt.getSeconds()
+    };
+}
+
+const getDayDir = (divider) => {
+    let dt = getDateObj()
+    let _divider = (typeof divider === 'string') ? divider : '/';
+    return dt.y + _divider + dt.m + _divider + dt.d + _divider
+}
+
+const imgUrlPrefix = (dir) => {
+    let dirname = (dir && typeof dir === 'string') ? dir + '/' : '';
+    if (dir && typeof dir === 'string') dirname += getDayDir()
+    return (PORT === 80) ? `${HTTP}://${HOST}/${dirname}` : `${HTTP}://${HOST}:${PORT}/${dirname}`;
+}
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+    storage: path.join(__dirname, '/' + SQL_DIR + '/database.sqlite')
+})
+
+//http://docs.sequelizejs.com/en/stable/docs/models-definition/
+const Img = sequelize.define('img', {
+    id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    option: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    category: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isUrl: true
+        }
+    }
+})
+const User = sequelize.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        },
+        unique: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: true
+    }
+})
+//Img.belongsTo(User, {foreignKey: 'userId'});
+sequelize.sync()
+Img.drop()
+Img.sync()
 
 // App
 const app = express()
@@ -16,7 +130,7 @@ const app = express()
 app.use((req, res, next) => {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8088')
+    res.setHeader('Access-Control-Allow-Origin', CORS_DOMAIN)
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
@@ -32,45 +146,98 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use('/uploads', express.static('uploads'))
+app.use('/' + UPLOAD_DIR, express.static(UPLOAD_DIR))
 
-app.get('/', function(req, res) {
-    res.send('Welcome to upload server!\n')
+app.get('/', (req, res) => {
+    Img.findAndCountAll({
+        where: {},
+        offset: 0,
+        limit: 10
+    }).then(result => {
+        console.log('result.count', result.count);
+        console.log('result.rows', result.rows);
+        let html = '<ul>'
+        for (let i = 0; i < result.count; i++) {
+            html += '<li>' + (i + 1) + '. ' + result.rows[i].dataValues.category + '<br><img src="' + result.rows[i].dataValues.url + '"><br>' + '![](' + result.rows[i].dataValues.url + ' "' + result.rows[i].dataValues.name + '")</li>'
+        }
+        html += '</ul>'
+        res.send('<h1>Welcome to Image Server! </h1>' + html)
+    })
 })
 
-app.post('/upload', function(req, res) {
-
+app.post('/' + UPLOAD_URL, (req, res) => {
+    let imgName;
     // create an incoming form object
-    var form = new formidable.IncomingForm()
+    let form = new formidable.IncomingForm()
 
     // specify that we want to allow the user to upload multiple files in a single request
     form.multiples = true
 
     // store all uploads in the /uploads directory
-    form.uploadDir = path.join(__dirname, '/uploads')
+    form.uploadDir = path.join(__dirname, '/' + UPLOAD_DIR + '/' + getDayDir())
+    console.log('form.uploadDir', form.uploadDir)
 
-    // every time a file has been uploaded successfully,
-    // rename it to it's orignal name
-    form.on('file', function(field, file) {
-        //console.log('file', file.path, form.uploadDir, file.name);
-        fs.rename(file.path, path.join(form.uploadDir, file.name) + '.png')
+    let promise = new Promise((resolve, reject) => {
+        mkdirp(form.uploadDir, (err) => {
+            if (err) {
+                console.error('mkdirp', err)
+                reject(err)
+            } else {
+                resolve(form.uploadDir);
+            }
+        })
     })
+    promise.then(result => {
+        console.log('promise ok', result)
+        // every time a file has been uploaded successfully,
+        // rename it to it's orignal name
+        form.on('file', (field, file) => {
+            //console.log('file', file.path, form.uploadDir, file.name);
+            imgName = `${IMG_PREFIX}${new Date().getTime()}` + '.png'
+            fs.rename(file.path, path.join(form.uploadDir, imgName))
+        })
 
-    // log any errors that occur
-    form.on('error', function(err) {
-        console.log('An error has occured: \n' + err)
+        // log any errors that occur
+        form.on('error', err => {
+            console.log('An error has occured: \n' + err)
+        })
+
+        // once all the files have been uploaded, send a response to the client
+        form.on('end', () => {
+            let url = imgUrlPrefix(UPLOAD_DIR) + imgName
+            Img.create({
+                name: imgName,
+                category: getDayDir(),
+                url: url
+            }).then(img => {
+                console.log('insert img', img.get('name'))
+            })
+
+            res.end(JSON.stringify({
+                img: url,
+                name: imgName,
+                category: getDayDir('-')
+            }))
+        })
+
+        // parse the incoming request containing the form data
+        // https://github.com/felixge/node-formidable
+        form.parse(req, (err, fields, files) => {
+            /*res.writeHead(200, {
+                'content-type': 'text/plain'
+            });
+            res.write('received upload:\n\n');
+            res.end(util.inspect({
+                fields: fields,
+                files: files
+            }));*/
+        })
+        //console.log('form', form);
+    }).catch(error => {
+        console.log('promise error', error)
     })
-
-    // once all the files have been uploaded, send a response to the client
-    form.on('end', function() {
-        res.end('success')
-    })
-
-    // parse the incoming request containing the form data
-    form.parse(req)
-    //console.log('form', form);
 
 })
 
 app.listen(PORT)
-console.log('Running on http://localhost:' + PORT)
+console.log('Running on ' + imgUrlPrefix())
